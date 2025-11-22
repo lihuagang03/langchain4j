d97e03bf97cfb9228d9972f17fc4141030ab0b54
lihuagang <bert825_work@163.com>
2025-11-22 13:54:32 +0800

    ChatMessage-对话消息 ChatMessageType-对话消息类型

--- langchain4j-core/src/main/java/dev/langchain4j/data/message/ChatMessageType.java
+++ langchain4j-core/src/main/java/dev/langchain4j/data/message/ChatMessageType.java
@@ -1,32 +1,39 @@
 package dev.langchain4j.data.message;
 
 /**
- * The type of content, e.g. text or image.
+ * 对话消息类型
+ * The type of chat message, e.g. system, user or ai.
  * Maps to implementations of {@link ChatMessage}.
  */
 public enum ChatMessageType {
     /**
      * A message from the system, typically defined by a developer.
+     * 系统消息，通常由开发者定义。
      */
     SYSTEM(SystemMessage.class),
 
     /**
      * A message from the user.
+     * 用户消息
      */
     USER(UserMessage.class),
 
     /**
      * A message from the AI.
+     * AI消息
      */
     AI(AiMessage.class),
 
     /**
      * A message from a tool.
+     * 工具消息
+     * 工具执行结果
      */
     TOOL_EXECUTION_RESULT(ToolExecutionResultMessage.class),
 
     /**
      * A custom message.
+     * 自定义消息
      */
     CUSTOM(CustomMessage.class);
 
--- langchain4j-core/src/main/java/dev/langchain4j/data/message/ContentType.java
+++ langchain4j-core/src/main/java/dev/langchain4j/data/message/ContentType.java
@@ -9,26 +9,31 @@ public enum ContentType {
 
     /**
      * Text content.
+     * 文本内容
      */
     TEXT(TextContent.class),
 
     /**
      * Image content.
+     * 图像内容
      */
     IMAGE(ImageContent.class),
 
     /**
      * Audio content.
+     * 音频内容
      */
     AUDIO(AudioContent.class),
 
     /**
      * Video content.
+     * 视频内容
      */
     VIDEO(VideoContent.class),
 
     /**
      * PDF file content.
+     * PDF 文件内容
      */
     PDF(PdfFileContent.class);
 
