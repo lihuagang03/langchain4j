766e6d19ad34b8a517fc8bbbb4be5f00f4239204
lihuagang <bert825_work@163.com>
2025-11-23 10:58:22 +0800

    DocumentTransformer-文档转换器，用于转换文档

--- langchain4j-core/src/main/java/dev/langchain4j/data/document/DocumentTransformer.java
+++ langchain4j-core/src/main/java/dev/langchain4j/data/document/DocumentTransformer.java
@@ -6,12 +6,15 @@ import java.util.Objects;
 import static java.util.stream.Collectors.toList;
 
 /**
+ * 文档转换器，用于转换文档。
+ * 实现可以执行各种任务，例如转换、筛选、丰富等。
  * Defines the interface for transforming a {@link Document}.
  * Implementations can perform a variety of tasks such as transforming, filtering, enriching, etc.
  */
 public interface DocumentTransformer {
 
     /**
+     * 转换提供的文档。
      * Transforms a provided document.
      *
      * @param document The document to be transformed.
