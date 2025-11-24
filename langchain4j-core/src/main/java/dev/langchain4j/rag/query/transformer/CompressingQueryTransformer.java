a5d1ebd8cac1a7082b03118a346193498ce8473e
lihuagang <bert825_work@163.com>
2025-11-24 14:18:47 +0800

    CompressingQueryTransformer-压缩查询转换器，利用聊天对话模型将给定的查询以及聊天记忆（之前的对话历史）压缩成一个简明的查询

--- langchain4j-core/src/main/java/dev/langchain4j/rag/query/transformer/CompressingQueryTransformer.java
+++ langchain4j-core/src/main/java/dev/langchain4j/rag/query/transformer/CompressingQueryTransformer.java
@@ -21,6 +21,8 @@ import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.joining;
 
 /**
+ * 压缩查询转换器，利用聊天对话模型将给定的查询以及聊天记忆（之前的对话历史）压缩成一个简明的查询。
+ * 只有在使用聊天记忆时才适用。
  * A {@link QueryTransformer} that leverages a {@link ChatModel} to condense a given {@link Query}
  * along with a chat memory (previous conversation history) into a concise {@link Query}.
  * This is applicable only when a {@link ChatMemory} is in use.
@@ -36,6 +38,23 @@ import static java.util.stream.Collectors.joining;
  */
 public class CompressingQueryTransformer implements QueryTransformer {
 
+    /**
+     * 默认的提示模版
+     */
+    /*
+     * 阅读并理解用户与人工智能之间的对话。
+     * 然后，分析用户的新查询。
+     * 从对话和新查询中识别所有相关的细节、术语和上下文。
+     * 将此查询重新表述为清晰、简明且自包含的格式，以便信息检索。
+     *
+     * 对话：
+     * {{chatMemory}}
+     *
+     * 用户查询：{{query}}
+     *
+     * 非常重要的是，你只能提供重新表述的查询，别的不要提供！
+     * 不要在查询前加上任何内容！
+     */
     public static final PromptTemplate DEFAULT_PROMPT_TEMPLATE = PromptTemplate.from(
             """
                     Read and understand the conversation between the User and the AI. \
@@ -52,7 +71,13 @@ public class CompressingQueryTransformer implements QueryTransformer {
                     Do not prepend a query with anything!"""
     );
 
+    /**
+     * 提示模版
+     */
     protected final PromptTemplate promptTemplate;
+    /**
+     * 聊天对话模型
+     */
     protected final ChatModel chatModel;
 
     public CompressingQueryTransformer(ChatModel chatModel) {
@@ -71,12 +96,14 @@ public class CompressingQueryTransformer implements QueryTransformer {
     @Override
     public Collection<Query> transform(Query query) {
 
+        // 聊天记忆
         List<ChatMessage> chatMemory = query.metadata().chatMemory();
         if (chatMemory.isEmpty()) {
             // no need to compress if there are no previous messages
             return singletonList(query);
         }
 
+        // 提示
         Prompt prompt = createPrompt(query, format(chatMemory));
         String compressedQueryText = chatModel.chat(prompt.text());
         Query compressedQuery = query.metadata() == null
@@ -86,6 +113,7 @@ public class CompressingQueryTransformer implements QueryTransformer {
     }
 
     protected String format(List<ChatMessage> chatMemory) {
+        // 聊天对话消息列表，分隔符：换行
         return chatMemory.stream()
                 .map(this::format)
                 .filter(Objects::nonNull)
@@ -93,12 +121,17 @@ public class CompressingQueryTransformer implements QueryTransformer {
     }
 
     protected String format(ChatMessage message) {
+        // 聊天对话消息
         if (message instanceof UserMessage userMessage) {
+            //
             return "User: " + userMessage.singleText();
         } else if (message instanceof AiMessage aiMessage) {
+            // AI消息，包含工具执行请求列表
             if (aiMessage.hasToolExecutionRequests()) {
+                // 工具执行请求列表
                 return null;
             }
+            // AI消息
             return "AI: " + aiMessage.text();
         } else {
             return null;
@@ -107,7 +140,9 @@ public class CompressingQueryTransformer implements QueryTransformer {
 
     protected Prompt createPrompt(Query query, String chatMemory) {
         Map<String, Object> variables = new HashMap<>();
+        // 查询文本
         variables.put("query", query.text());
+        // 聊天记忆
         variables.put("chatMemory", chatMemory);
         return promptTemplate.apply(variables);
     }
