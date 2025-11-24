04f7cdbda088031fef1ae766b0b1acf4bfbfd85d
lihuagang <bert825_work@163.com>
2025-11-24 14:48:17 +0800

    ContentRetriever-内容检索器，使用给定的查询从底层数据源检索内容

--- langchain4j-core/src/main/java/dev/langchain4j/rag/content/retriever/ContentRetriever.java
+++ langchain4j-core/src/main/java/dev/langchain4j/rag/content/retriever/ContentRetriever.java
@@ -6,19 +6,25 @@ import dev.langchain4j.rag.query.Query;
 import java.util.List;
 
 /**
- * 内容检索器
+ * 内容检索器，使用给定的查询从底层数据源检索内容。
  * Retrieves {@link Content}s from an underlying data source using a given {@link Query}.
  * <br>
+ * 目标是仅检索与给定查询相关的内容。
  * The goal is to retrieve only relevant {@link Content}s in relation to a given {@link Query}.
  * <br>
+ * 底层数据源几乎可以是任何东西：
  * The underlying data source can be virtually anything:
  * <pre>
- * - Embedding (vector) store (see {@link EmbeddingStoreContentRetriever})
+ * - Embedding (vector) store (see {@link EmbeddingStoreContentRetriever}) 嵌入（向量）存储
  * - Full-text search engine (see {@code AzureAiSearchContentRetriever} in the {@code langchain4j-azure-ai-search} module)
+ *   全文搜索引擎
  * - Hybrid of vector and full-text search (see {@code AzureAiSearchContentRetriever} in the {@code langchain4j-azure-ai-search} module)
- * - Web Search Engine (see {@link WebSearchContentRetriever})
+ *   向量搜索和全文搜索的混合
+ * - Web Search Engine (see {@link WebSearchContentRetriever}) 网络搜索引擎
  * - Knowledge graph (see {@code Neo4jContentRetriever} in the {@code langchain4j-community-neo4j-retriever} module)
+ *   知识图谱
  * - SQL database (see {@code SqlDatabaseContentRetriever} in the {@code langchain4j-experimental-sql} module)
+ *   SQL数据库
  * - etc.
  * </pre>
  *
@@ -28,6 +34,8 @@ import java.util.List;
 public interface ContentRetriever {
 
     /**
+     * 使用给定的查询检索相关内容。
+     * 内容按相关性排序，最相关的内容会出现在返回的内容列表的开头。
      * Retrieves relevant {@link Content}s using a given {@link Query}.
      * The {@link Content}s are sorted by relevance, with the most relevant {@link Content}s appearing
      * at the beginning of the returned {@code List<Content>}.
