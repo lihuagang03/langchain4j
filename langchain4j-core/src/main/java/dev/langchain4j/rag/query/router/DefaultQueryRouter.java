88ce49d258b46fc7991b4bae764983be2e710741
lihuagang <bert825_work@163.com>
2025-11-24 14:41:52 +0800

    QueryRouter-查询路由器，将给定的查询路由到一个或多个内容检索器 DefaultQueryRouter

--- langchain4j-core/src/main/java/dev/langchain4j/rag/query/router/DefaultQueryRouter.java
+++ langchain4j-core/src/main/java/dev/langchain4j/rag/query/router/DefaultQueryRouter.java
@@ -10,15 +10,20 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableCollection;
 
 /**
+ * 查询路由器的默认实现，旨在适用于大多数使用场景。
  * Default implementation of {@link QueryRouter} intended to be suitable for the majority of use cases.
  * <br>
  * <br>
+ * 重要的是要注意，尽管会尽量避免引入破坏性更改，
+ * 但如果发现当前行为无法充分满足大多数使用场景，这个类的默认行为未来可能会被更新。
+ * 这些更改将旨在惠及当前和未来的用户。
  * It's important to note that while efforts will be made to avoid breaking changes,
  * the default behavior of this class may be updated in the future if it's found
  * that the current behavior does not adequately serve the majority of use cases.
  * Such changes would be made to benefit both current and future users.
  * <br>
  * <br>
+ * 本实现始终将所有查询路由到构造函数中提供的一个或多个内容检索器。
  * This implementation always routes all {@link Query}s
  * to one or multiple {@link ContentRetriever}s provided in the constructor.
  *
@@ -26,6 +31,9 @@ import static java.util.Collections.unmodifiableCollection;
  */
 public class DefaultQueryRouter implements QueryRouter {
 
+    /**
+     * 内容检索器列表
+     */
     private final Collection<ContentRetriever> contentRetrievers;
 
     public DefaultQueryRouter(ContentRetriever... contentRetrievers) {
--- langchain4j-core/src/main/java/dev/langchain4j/rag/query/router/QueryRouter.java
+++ langchain4j-core/src/main/java/dev/langchain4j/rag/query/router/QueryRouter.java
@@ -8,16 +8,21 @@ import dev.langchain4j.rag.query.Query;
 import java.util.Collection;
 
 /**
+ * 查询路由器，将给定的查询路由到一个或多个内容检索器。
  * Routes the given {@link Query} to one or multiple {@link ContentRetriever}s.
  * <br>
+ * 目标是确保内容仅从相关的数据源中获取。
  * The goal is to ensure that {@link Content} is retrieved only from relevant data sources.
  * <br>
+ * 一些潜在的方法包括：
  * Some potential approaches include:
  * <pre>
- * - Using an LLM (see {@link LanguageModelQueryRouter})
+ * - Using an LLM (see {@link LanguageModelQueryRouter}) 使用大型语言模型
  * - Using an {@link EmbeddingModel} (aka "semantic routing", see {@code EmbeddingModelTextClassifier} in the {@code langchain4j} module)
- * - Using keyword-based routing
+ *   使用嵌入模型（也称为“语义路由”）
+ * - Using keyword-based routing 使用基于关键字的路由
  * - Route depending on the user ({@code query.metadata().chatMemoryId()}) and/or permissions
+ *   路由取决于用户（query.metadata().chatMemoryId()）和/或权限
  * </pre>
  *
  * @see DefaultQueryRouter
@@ -26,6 +31,7 @@ import java.util.Collection;
 public interface QueryRouter {
 
     /**
+     * 将给定的查询路由到一个或多个内容检索器。
      * Routes the given {@link Query} to one or multiple {@link ContentRetriever}s.
      *
      * @param query The {@link Query} to be routed.
