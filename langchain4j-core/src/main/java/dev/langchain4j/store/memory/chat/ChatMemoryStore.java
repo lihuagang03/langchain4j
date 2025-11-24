d423f69850433f6cac2d36fe34a20983e541d5aa
lihuagang <bert825_work@163.com>
2025-11-24 17:01:24 +0800

    ChatMemoryStore-聊天记忆存储 InMemoryChatMemoryStore

--- langchain4j-core/src/main/java/dev/langchain4j/store/memory/chat/ChatMemoryStore.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/memory/chat/ChatMemoryStore.java
@@ -8,10 +8,16 @@ import dev.langchain4j.memory.ChatMemory;
 import java.util.List;
 
 /**
+ * 聊天记忆存储
+ * 表示用于聊天记忆状态的存储。
+ * 允许在存储聊天记忆的位置和方式上具有灵活性。
  * Represents a store for the {@link ChatMemory} state.
  * Allows for flexibility in terms of where and how chat memory is stored.
  * <br>
  * <br>
+ * 目前，唯一可用的实现是 InMemoryChatMemoryStore。
+ * 随着时间的推移，将会为流行的存储类型（如 SQL 数据库、文档存储等）添加开箱即用的实现。
+ * 与此同时，您可以实现该接口以连接到任何您选择的存储。
  * Currently, the only implementation available is {@link InMemoryChatMemoryStore}.
  * Over time, out-of-the-box implementations will be added for popular stores like SQL databases, document stores, etc.
  * In the meantime, you can implement this interface to connect to any storage of your choice.
@@ -22,14 +28,16 @@ import java.util.List;
 public interface ChatMemoryStore {
 
     /**
+     * 检索指定聊天记录的消息。
      * Retrieves messages for a specified chat memory.
      *
-     * @param memoryId The ID of the chat memory.
+     * @param memoryId The ID of the chat memory. 聊天记忆ID
      * @return List of messages for the specified chat memory. Must not be null. Can be deserialized from JSON using {@link ChatMessageDeserializer}.
      */
     List<ChatMessage> getMessages(Object memoryId);
 
     /**
+     * 更新指定聊天记录的消息。
      * Updates messages for a specified chat memory.
      *
      * @param memoryId The ID of the chat memory.
--- langchain4j-core/src/main/java/dev/langchain4j/store/memory/chat/InMemoryChatMemoryStore.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/memory/chat/InMemoryChatMemoryStore.java
@@ -8,8 +8,11 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * 聊天记忆内存存储
+ * ChatMemoryStore 的实现，用于将 ChatMemory（聊天消息）的状态存储在内存中。
  * Implementation of {@link ChatMemoryStore} that stores state of {@link dev.langchain4j.memory.ChatMemory} (chat messages) in-memory.
  * <p>
+ * 这种存储机制是临时的，无法在应用重启后保留数据。
  * This storage mechanism is transient and does not persist data across application restarts.
  */
 public class InMemoryChatMemoryStore implements ChatMemoryStore {
