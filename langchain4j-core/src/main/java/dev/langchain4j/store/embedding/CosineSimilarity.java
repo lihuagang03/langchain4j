47f4a0f12e41fa5ac5379d617367a7df4b51247b
lihuagang <bert825_work@163.com>
2025-12-12 23:48:43 +0800

    CosineSimilarity-余弦相似度 RelevanceScore-相关度评分/相关性得分

--- langchain4j-core/src/main/java/dev/langchain4j/store/embedding/CosineSimilarity.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/embedding/CosineSimilarity.java
@@ -6,31 +6,41 @@ import static dev.langchain4j.internal.Exceptions.illegalArgument;
 import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
 
 /**
- * 计算两个向量之间的余弦相似度。
+ * 余弦相似度
+ * 用于计算两个向量之间余弦相似度的工具类。
  * Utility class for calculating cosine similarity between two vectors.
  */
 public class CosineSimilarity {
     private CosineSimilarity() {}
 
     /**
+     * 一个用于避免除以零的小值。
      * A small value to avoid division by zero.
      */
     public static final float EPSILON = 1e-8f;
 
     /**
+     * 计算两个向量之间的余弦相似度。
      * Calculates cosine similarity between two vectors.
      * <p>
+     * 余弦相似度衡量两个向量之间夹角的余弦值，用以表示它们的方向相似性。
+     * 它产生的值的范围为：
      * Cosine similarity measures the cosine of the angle between two vectors, indicating their directional similarity.
      * It produces a value in the range:
      * <p>
+     * -1 表示向量完全相反（方向相反）。
      * -1 indicates vectors are diametrically opposed (opposite directions).
      * <p>
+     * 0 表示向量正交（没有方向相似性）。
      * 0 indicates vectors are orthogonal (no directional similarity).
      * <p>
+     * 1 表示向量指向相同方向（但不一定大小相同）。
      * 1 indicates vectors are pointing in the same direction (but not necessarily of the same magnitude).
      * <p>
+     * 不要将其与余弦距离（[0..2]）混淆，余弦距离用来量化两个向量的差异。
      * Not to be confused with cosine distance ([0..2]), which quantifies how different two vectors are.
      * <p>
+     * 全零向量的嵌入被认为与所有其他向量正交；包括其他全零向量。
      * Embeddings of all-zeros vectors are considered orthogonal to all other vectors;
      * including other all-zeros vectors.
      *
@@ -65,6 +75,7 @@ public class CosineSimilarity {
     }
 
     /**
+     * 将相关性分数转换为余弦相似度。
      * Converts relevance score into cosine similarity.
      *
      * @param relevanceScore Relevance score in the range [0..1] where 0 is not relevant and 1 is relevant.
--- langchain4j-core/src/main/java/dev/langchain4j/store/embedding/RelevanceScore.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/embedding/RelevanceScore.java
@@ -1,12 +1,15 @@
 package dev.langchain4j.store.embedding;
 
 /**
+ * 相关度评分/相关性得分
+ * 用于在余弦相似度和相关性得分之间转换的工具类。
  * Utility class for converting between cosine similarity and relevance score.
  */
 public class RelevanceScore {
     private RelevanceScore() {}
 
     /**
+     * 将余弦相似度转换为相关度评分。
      * Converts cosine similarity into relevance score.
      *
      * @param cosineSimilarity Cosine similarity in the range [-1..1] where -1 is not relevant and 1 is relevant.
