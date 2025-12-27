b50ea171226f338ae8ea359c33afb168846b3b4c
lihuagang03 <bert825_work@163.com>
2025-12-28 01:21:43 +0800

    SupervisorContextStrategy-主管上下文策略 SupervisorResponseStrategy-主管响应策略

--- langchain4j-agentic/src/main/java/dev/langchain4j/agentic/supervisor/SupervisorContextStrategy.java
+++ langchain4j-agentic/src/main/java/dev/langchain4j/agentic/supervisor/SupervisorContextStrategy.java
@@ -3,18 +3,23 @@ package dev.langchain4j.agentic.supervisor;
 import dev.langchain4j.memory.ChatMemory;
 
 /**
+ * 主管上下文策略
+ * 为主管智能体提供上下文的策略。
  * Strategy for providing context to the supervisor agent.
  */
 public enum SupervisorContextStrategy {
     /**
+     * 仅使用主管的聊天记忆（默认）。
      * Use only the supervisors {@link ChatMemory} (default).
      */
     CHAT_MEMORY,
     /**
+     * 仅使用主管与其子智能体之间互动的摘要。
      * Use only a summarization of the interaction of the supervisor with its sub-agents.
      */
     SUMMARIZATION,
     /**
+     * 同时使用主管的聊天记忆和主管与其子智能体互动的总结。
      * Use both the supervisor's {@link ChatMemory} and a summarization of the interaction of the supervisor with its sub-agents.
      */
     CHAT_MEMORY_AND_SUMMARIZATION
--- langchain4j-agentic/src/main/java/dev/langchain4j/agentic/supervisor/SupervisorResponseStrategy.java
+++ langchain4j-agentic/src/main/java/dev/langchain4j/agentic/supervisor/SupervisorResponseStrategy.java
@@ -1,19 +1,24 @@
 package dev.langchain4j.agentic.supervisor;
 
 /**
+ * 主管响应策略
+ * 决定主管智能体应返回哪个响应的策略。
  * Strategy to decide which response the supervisor agent should return.
  */
 public enum SupervisorResponseStrategy {
     /**
+     * 使用内部大语言模型对上一次回复以及主管与其子智能体互动的总结进行评分，评估其是否符合原始用户请求，并返回得分较高的那一个。
      * Use an internal LLM to score the last response and the summarization of the interaction of the supervisor
      * with its sub-agents against the original user request, and return the one with the higher score.
      */
     SCORED,
     /**
+     * 返回主管与其子智能体互动的总结。
      * Return a summarization of the interaction of the supervisor with its sub-agents.
      */
     SUMMARY,
     /**
+     * 仅返回最后调用的子智能体的最终响应（默认）。
      * Return only the final response of the last invoked sub-agent (default).
      */
     LAST
